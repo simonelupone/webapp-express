f7b87a6dd22231e7ab4c7f43c761e905ae7020f4
simonelupone <simone.lupone@gmail.com>
2025-06-03 16:41:52 +0200

    feat: add error handling middleware to manage server errors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const movieRouter = require("./routers/movieRouter");
 
 // import middleware
 const imagePathMiddleware = require("./middlewares/imagePath");
+const errorsHandler = require("./middlewares/errorsHandler");
 
 // static-assets middleware
 app.use(express.json("public"));
@@ -19,6 +20,8 @@ app.use(imagePathMiddleware);
 app.get("/", (req, res) => {
   res.send("Movies API Server");
 });
+
+app.use(errorsHandler);
 
 app.use("/api/movies", movieRouter);
 
---
+++ middlewares/errorsHandler.js
@@ -0,0 +1,7 @@
+const errorsHandler = (err, req, res, next) => {
+  res.status(500);
+  res.json({
+    error: err.message,
+  });
+};
+module.exports = errorsHandler;
